74ea66e79d9d57771644632639c81e2a3a1d675e
Rafael JPD <rafael.pezzuto@gmail.com>
2019-07-14 12:14:56 -0300

    add: string processor to normalize characters

---
+++ utils/string_processor.py
@@ -0,0 +1,31 @@
+import unicodedata
+
+
+class StringProcessor(object):
+
+    @staticmethod
+    def remove_accents(text):
+        return unicodedata.normalize('NFKD', text).encode('ASCII', 'ignore').decode('ASCII')
+
+
+    @staticmethod
+    def alpha_num_space(text):
+        new_str = []
+        for character in text:
+            if character.isalnum() or character.isspace():
+                new_str.append(character)
+            else:
+                new_str.append(' ')
+        return ''.join(new_str)
+
+
+    @staticmethod
+    def remove_double_spaces(text):
+        while '  ' in text:
+            text = text.replace('  ', ' ')
+        return text.strip()
+
+
+    @staticmethod
+    def preprocess_name(text):
+        return StringProcessor.remove_double_spaces(StringProcessor.alpha_num_space(StringProcessor.remove_accents(text)))
