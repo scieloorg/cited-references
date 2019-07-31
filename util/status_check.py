1676833eb87d214d50cbdf62373b7112e5d2eb65
Rafael JPD <rafael.pezzuto@gmail.com>
2019-07-31 18:41:46 -0300

    add: status by reference type article

--- util/status_check.py
+++ util/status_check.py
@@ -6,8 +6,9 @@ import sys
 
 if __name__ == "__main__":
     DATABASE_NAME = sys.argv[1]
+    MODO = sys.argv[2]
     
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         print('Error: enter database name')
         sys.exit(1)
 
@@ -15,11 +16,22 @@ if __name__ == "__main__":
     database = client[DATABASE_NAME]
 
     print('col\ttotal\tdoi\t+1\t-1\t-2\tmissing')
-    for i in database.list_collection_names():
-        total_docs = database[i].count_documents({})
-        with_doi = database[i].count_documents({'v237': {'$exists': True}})
-        status_1_minus = database[i].count_documents({'status': -1})
-        status_1_plus = database[i].count_documents({'status': 1})
-        status_2_minus = database[i].count_documents({'status': -2})
-        missing = with_doi - sum([status_1_minus, status_1_plus])
-        print(i, total_docs, with_doi, status_1_plus, status_1_minus, status_2_minus, missing, sep='\t')
+
+    if MODO != 'article':
+        for i in database.list_collection_names():
+            total_docs = database[i].count_documents({})
+            with_doi = database[i].count_documents({'v237': {'$exists': True}})
+            status_1_minus = database[i].count_documents({'status': -1})
+            status_1_plus = database[i].count_documents({'status': 1})
+            status_2_minus = database[i].count_documents({'status': -2})
+            missing = with_doi - sum([status_1_minus, status_1_plus])
+            print(i, total_docs, with_doi, status_1_plus, status_1_minus, status_2_minus, missing, sep='\t')
+    else:
+        for i in database.list_collection_names():
+            total_docs = database[i].count({'v709': [ {'_': 'article'} ]})
+            with_doi = database[i].count({'$and': [{'v237': {'$exists': True}}, {'v709': [ {'_': 'article'} ]}]})
+            status_1_minus = database[i].count({'$and': [{'status': -1}, {'v709': [ {'_': 'article'} ]}]})
+            status_1_plus = database[i].count({'$and': [{'status': 1}, {'v709': [ {'_': 'article'} ]}]})
+            status_2_minus = database[i].count({'$and': [{'status': -2}, {'v709': [ {'_': 'article'} ]}]})          
+            missing = with_doi - sum([status_1_minus, status_1_plus])
+            print(i, total_docs, with_doi, status_1_plus, status_1_minus, status_2_minus, missing, sep='\t')
