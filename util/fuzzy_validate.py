f6410a7b998fa13a30d07d34c4363c045088b17b
Rafael J P D <rafael.pezzuto@gmail.com>
2020-01-07 21:30:02 -0300

    imp: fuzzy match now validates with 3rd base

--- util/fuzzy_validate.py
+++ util/fuzzy_validate.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+
 def read_fuzzy_data(path_file_fuzzy_data):
     fd = open(path_file_fuzzy_data)
     data = [d.strip().split('|') for d in fd]
@@ -39,12 +42,11 @@ def extract_l2_dicts(path_issnl2all_base: str):
     return l2is, l2titles
 
 
-def load_year_volume_base(path_year_volume_base: str, issn2issnl: dict):
-    print('loading year_volume_base')
-    year_volume_base = [i.split('|') for i in open(path_year_volume_base)]
+def load_year_volume_base_2nd(path_year_volume_base: str, issn2issnl: dict):
+    print('loading secondary base')
+    yv2 = [i.strip().split('|') for i in open(path_year_volume_base)]
     issn_year_volume2issnl = {}
-    for r in year_volume_base:
-        # title = r[1].strip()
+    for r in yv2:
         issn = r[0].strip().replace('-', '')
         normalized_issnl = issn2issnl.get(issn, '')
         if normalized_issnl == '':
@@ -56,44 +58,59 @@ def load_year_volume_base(path_year_volume_base: str, issn2issnl: dict):
             issn_year_volume2issnl[mkey] = {normalized_issnl}
         else:
             issn_year_volume2issnl[mkey].add(normalized_issnl)
-    del year_volume_base
+    del yv2
+    return issn_year_volume2issnl
+
+
+def load_year_volume_base_3rd(path_year_volume_base: str):
+    print('loading terciary base')
+    yv3 = [i.strip().split('|') for i in open(path_year_volume_base)][1:]
+    issn_year_volume2issnl = {}
+    for r in yv3:
+        issnl = r[0]
+        issn = r[1]
+        year = r[2]
+        predicted_vols = [v for v in [r[4], r[5], r[6]] if int(v) > 0]
+
+        for pv in predicted_vols:
+            mkey = '|'.join([issn, year, pv])
+            if mkey not in issn_year_volume2issnl:
+                issn_year_volume2issnl[mkey] = {issnl}
+            else:
+                issn_year_volume2issnl[mkey].add(issnl)
+    del yv3
     return issn_year_volume2issnl
 
 
 def validate(match, iyv2issnl, l2is):
-    col = match[0]
-    id_ref = match[1]
-    # title = match[2]
     year = match[3]
     if len(year) > 4:
         year = year[:4]
     volume = match[4]
     issnls = match[5].split('#')
-    # n = match[6]
 
     vs = set()
     for l in issnls:
         l_issns = l2is[l]
-        # l_titles = l2titles[l]
         for i in l_issns:
-        # for t in l_titles:
             k = '|'.join([i, year, volume])
             if k in iyv2issnl:
                 v = '#'.join(iyv2issnl[k])
                 if len(iyv2issnl[k]) != 1:
                     print(iyv2issnl[k])
                 vs.add(v)
-    return vs
+    if len(vs) == 0:
+        vs = {'-1'}
+    return list(vs)
 
 
 def save_results(res):
-    f = open('/home/rafael/valid_matches.csv', 'w')
+    f = open('fuzzy_11_12_tested.csv', 'w')
+    f.write('col|ref_id|titulo|year|volume|issns|n_issns|valid_2nd|valid_3rd\n')
     for r in res:
-        col = r[0][0]
-        id_ref = r[0][1]
-        title = r[0][2]
-        issnls = '#'.join(r[1])
-        fr = '|'.join([col, id_ref, title, issnls])
+        issnls_2nd = '#'.join(r[1])
+        issnls_3rd = '#'.join(r[2])
+        fr = '|'.join(r[0] + [issnls_2nd, issnls_3rd])
         f.write(fr + '\n')
     f.close()
 
@@ -101,19 +118,30 @@ def save_results(res):
 def main():
     path_fuzzy = '/home/rafael/Temp/scielo/analysis/2019-12/matches_11_12/fuzzy_11_12.tsv'
     path_issnl2all = '/home/rafael/Temp/scielo/bases/primaria/base_issnl2all_v0.4.csv'
-    path_year_volume = '/home/rafael/Temp/scielo/bases/secundaria/base_year_volume_v0.3.csv'
+    path_year_volume_2nd = '/home/rafael/Temp/scielo/bases/secundaria/base_year_volume_v0.3.csv'
+    path_year_volume_3rd = '/home/rafael/Temp/scielo/bases/terciaria/base_pvol_issn_v0.1.csv'
 
     data = read_fuzzy_data(path_fuzzy)
     i2l, i2titles = extract_i2_dicts(path_issnl2all)
     del i2titles
     l2is, l2titles = extract_l2_dicts(path_issnl2all)
-    iyv2issnl = load_year_volume_base(path_year_volume, i2l)
+    iyv2issnl_2nd = load_year_volume_base_2nd(path_year_volume_2nd, i2l)
     del i2l
+    iyv2issnl_3rd = load_year_volume_base_3rd(path_year_volume_3rd)
 
+    print('validating with secondary base')
     results = []
     for i, d in enumerate(data):
         print('\r%d/%d' % (i + 1, len(data)), end='')
-        results.append([d[:3], validate(d, iyv2issnl, l2is)])
+        results.append([d, validate(d, iyv2issnl_2nd, l2is)])
+    print('')
+
+    print('validating with terciary base')
+    for i, d in enumerate(results):
+        print('\r%d/%d' % (i + 1, len(results)), end='')
+        d.append(validate(d[0], iyv2issnl_3rd, l2is))
+    print('')
+
     save_results(results)
 
 
