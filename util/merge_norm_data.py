f091f33d6179bbc2c33116055ab13de9e9836cc0
Rafael J P D <rafael.pezzuto@gmail.com>
2020-04-03 18:53:43 -0300

    Adicionado exportadores de resultados de estagios exato 2 e fuzzy 1

--- util/merge_norm_data.py
+++ util/merge_norm_data.py
@@ -1,4 +1,5 @@
 import json
+import os
 import zipfile
 
 
@@ -60,16 +61,16 @@ def load_db1(file_name):
 
 
 def merge_dicts(d1, d2):
-    d3 = {'metadata': d1['metadata']}
+    d12 = {'metadata': d1['metadata']}
 
     for k in d2['metadata']:
         v_id = d2['metadata'][k]
-        if k not in d3['metadata']:
-            d3['metadata'][k] = v_id
+        if k not in d12['metadata']:
+            d12['metadata'][k] = v_id
         else:
             print('%s ja esta no dict' % k)
 
-    return d3
+    return d12
 
 
 def main():
@@ -82,24 +83,35 @@ def main():
     for col in collections:
 
         try:
-            fcrossref = open('/home/rafael/crossref_' + col + '.json')
-            fexact_db1 = open('/home/rafael/exact_db1_' + col + '.json')
+            fcrossref = open('/home/rafael/matches-2019-12/crossref/crossref_' + col + '.json')
+            fexact_db1 = open('/home/rafael/matches-2019-12/exact_db1/exact_db1_' + col + '.json')
+            fexact_db2 = open('/home/rafael/matches-2019-12/exact_db2/exact_db2_' + col + '.json')
+            ffuzzy_db1 = open('/home/rafael/matches-2019-12/fuzzy_db1/fuzzy_db1_' + col + '.json')
 
             d1 = json.load(fcrossref)
             d2 = json.load(fexact_db1)
+            d3 = json.load(fexact_db2)
+            d4 = json.load(ffuzzy_db1)
 
             norm_d1 = add_norm_status(d1, '1')
             norm_d2 = add_norm_status(d2, '2')
+            norm_d3 = add_norm_status(d3, '3')
+            norm_d4 = add_norm_status(d4, '4')
 
             norm_d2 = add_bc1_data(norm_d2, i2l, l2is, l2t)
+            norm_d3 = add_bc1_data(norm_d3, i2l, l2is, l2t)
+            norm_d4 = add_bc1_data(norm_d4, i2l, l2is, l2t)
 
-            d3 = merge_dicts(norm_d1, norm_d2)
+            d12 = merge_dicts(norm_d1, norm_d2)
+            d123 = merge_dicts(d12, norm_d3)
+            d1234 = merge_dicts(d123, norm_d4)
 
-            json.dump(d3, open('/home/rafael/external_metadata_' + col + '.json', 'w'), indent='\t', sort_keys=True)
+            json.dump(d1234, open('/home/rafael/matches-2019-12/external_metadata_' + col + '.json', 'w'))
 
-            zip_file = zipfile.ZipFile('/home/rafael/external_metadata_' + col + '.json.zip', 'w')
-            zip_file.write('/home/rafael/external_metadata_' + col + '.json', 'external_metadata_' + col + '.json', compress_type=zipfile.ZIP_DEFLATED)
+            zip_file = zipfile.ZipFile('/home/rafael/matches-2019-12/external_metadata_' + col + '.json.zip', 'w')
+            zip_file.write('/home/rafael/matches-2019-12/external_metadata_' + col + '.json', 'external_metadata_' + col + '.json', compress_type=zipfile.ZIP_DEFLATED)
             zip_file.close()
+            os.remove('/home/rafael/matches-2019-12/external_metadata_' + col + '.json')
         except FileNotFoundError as e:
             print(e)
 
